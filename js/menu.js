0df8876d6e1f1aed14c14e75d139a4c744300ca1
honza_b <benedikj.jan@gmail.com>
2015-05-19 01:44:42 +0200

    sections, auto scroll, part of header

---
+++ js/menu.js
@@ -0,0 +1,11 @@
+$('.navbar-nav a').click(function(){
+    if( $(this).attr('href') === '#services' ) {
+        s.animateTo(1000);
+    } else if( $(this).attr('href') === '#foto' ) {
+        s.animateTo(2000);
+    } else {
+        s.animateTo(0);
+    }
+    //toggleMenu();
+    return false;
+});
